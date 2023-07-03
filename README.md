3eaf1f5caa1f17947cb13a73269cc5f92e8195c0
msrocka <michael.srocka@gmail.com>
2023-07-03 16:43:55 +0200

    also segfault with i64

--- README.md
+++ README.md
@@ -3,7 +3,42 @@
 * https://github.com/doraneko94/pardiso-sys
 * https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=caa5ecbe645fcf37fe2dc2538f6437f2ac09325f
 * https://www.intel.com/content/www/us/en/docs/onemkl/developer-reference-c/2023-0/pardiso.html#GUID-431916D5-B76D-48A1-ABB5-1A0613FDC0FAhttps://www.intel.com/content/www/us/en/docs/onemkl/developer-reference-c/2023-0/pardiso.html#GUID-431916D5-B76D-48A1-ABB5-1A0613FDC0FA
-*
+
+## References
+
+### Official doc:
+
+* Intel: https://www.intel.com/content/www/us/en/docs/onemkl/developer-reference-c/2023-0/pardiso.html#GUID-431916D5-B76D-48A1-ABB5-1A0613FDC0FA
+
+* Old guide: https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=caa5ecbe645fcf37fe2dc2538f6437f2ac09325f
+
+### Linking examples
+
+* Python: https://github.com/haasad/PyPardisoProject
+* Java: https://github.com/bytedeco/javacpp-presets/tree/master/mkl
+* Rust: https://github.com/doraneko94/pardiso-sys
+* C: https://github.com/ww382/CS5220FinalProject/blob/master/pardiso_sym_c.c
+* Julia: https://github.com/JuliaSparse/Pardiso.jl
+
+### FFI
+
+* Julia: https://docs.julialang.org/en/v1/manual/calling-c-and-fortran-code/
+
+
+### AMD
+
+Seems that adding a function `mkl_serv_intel_cpu_true` could be used to call the optimized routines also on AMD:
+
+```c
+int mkl_serv_intel_cpu_true() {
+  return 1;
+}
+```
+
+* https://danieldk.eu/mkl-amd-zen/
+* https://scrp.econ.cuhk.edu.hk/blog/analysis/2022/02/07/mkl-optimization.html
+
+
 ## TODO
 
 * build tool
--- test.jl
+++ test.jl
@@ -1,37 +1,39 @@
-pt = zeros(Int, 64)
-maxfct = Ref{Cint}(1)
-mnum = Ref{Cint}(1)
-mtype = Ref{Cint}(11)
-phase = Ref{Cint}(13)
-nrhs = Ref{Cint}(1)
-msglvl = Ref{Cint}(1)
-error = Ref{Cint}(0)
+const MklInt = Int64
 
-n = Ref{Cint}(5)
-ia = Vector{Int32}([1, 4, 6, 9, 12, 14])
-ja = Vector{Int32}([1, 2, 4, 1, 2, 3, 4, 5, 1, 3, 4, 2, 5])
+pt = zeros(Int64, 64)
+maxfct = Ref{MklInt}(1)
+mnum = Ref{MklInt}(1)
+mtype = Ref{MklInt}(11)
+phase = Ref{MklInt}(13)
+nrhs = Ref{MklInt}(1)
+msglvl = Ref{MklInt}(1)
+error = Ref{MklInt}(0)
+
+n = Ref{MklInt}(5)
+ia = Vector{MklInt}([1, 4, 6, 9, 12, 14])
+ja = Vector{MklInt}([1, 2, 4, 1, 2, 3, 4, 5, 1, 3, 4, 2, 5])
 a = Vector{Float64}([1.0, -1.0, -3.0, -2.0, 5.0, 4.0, 6.0, 4.0, -4.0, 2.0, 7.0, 8.0, -5.0 ])
 b = Vector{Float64}([1.0, 1.0, 1.0, 1.0, 1.0])
 x = zeros(Float64, 5)
 
-perm = Int32[]
-iparm = zeros(Int32, 64)
+perm = MklInt[]
+iparm = zeros(MklInt, 64)
 
 @ccall "./bin/mkl_rt.2.dll".pardiso(
-  pt::Ptr{Int},
-  maxfct::Ptr{Cint},
-  mnum::Ptr{Cint},
-  mtype::Ptr{Cint},
-  phase::Ptr{Cint},
-  n::Ptr{Cint},
+  pt::Ptr{Int64},
+  maxfct::Ptr{MklInt},
+  mnum::Ptr{MklInt},
+  mtype::Ptr{MklInt},
+  phase::Ptr{MklInt},
+  n::Ptr{MklInt},
   a::Ptr{Float64},
-  ia::Ptr{Int32},
-  ja::Ptr{Int32},
-  perm::Ptr{Int32},
-  nrhs::Ptr{Cint},
-  iparm::Ptr{Int32},
-  msglvl::Ptr{Cint},
+  ia::Ptr{MklInt},
+  ja::Ptr{MklInt},
+  perm::Ptr{MklInt},
+  nrhs::Ptr{MklInt},
+  iparm::Ptr{MklInt},
+  msglvl::Ptr{MklInt},
   b::Ptr{Float64},
   x::Ptr{Float64},
-  error::Ptr{Cint}
+  error::Ptr{MklInt}
 )::Cvoid
