0fc0ffa9e13ca8bb64c9b97979ca0c9992cb716c
msrocka <michael.srocka@gmail.com>
2023-07-04 15:58:10 +0200

    next try, no luck

--- test.jl
+++ test.jl
@@ -4,20 +4,28 @@ pt = zeros(Int64, 64)
 maxfct = Ref{MklInt}(1)
 mnum = Ref{MklInt}(1)
 mtype = Ref{MklInt}(11)
-phase = Ref{MklInt}(13)
+phase = Ref{MklInt}(11)
+
+n = Ref{MklInt}(2)
+a = Vector{Float64}([1.0, -0.5, -1.0, 1.0])
+ia = Vector{MklInt}([1, 3, 5])
+ja = Vector{MklInt}([1, 2, 1, 2])
+
+perm = zeros(MklInt, 2)
 nrhs = Ref{MklInt}(1)
+iparm = zeros(MklInt, 64)
 msglvl = Ref{MklInt}(1)
+
+b = Vector{Float64}([1.0, 0.0])
+x = zeros(Float64, 2)
+
 error = Ref{MklInt}(0)
 
-n = Ref{MklInt}(5)
-ia = Vector{MklInt}([1, 4, 6, 9, 12, 14])
-ja = Vector{MklInt}([1, 2, 4, 1, 2, 3, 4, 5, 1, 3, 4, 2, 5])
-a = Vector{Float64}([1.0, -1.0, -3.0, -2.0, 5.0, 4.0, 6.0, 4.0, -4.0, 2.0, 7.0, 8.0, -5.0 ])
-b = Vector{Float64}([1.0, 1.0, 1.0, 1.0, 1.0])
-x = zeros(Float64, 5)
+@ccall "./bin/mkl_rt.2.dll".pardisoinit(
+  pt::Ptr{Int64},
+  mtype::Ptr{MklInt},
+  iparm::Ptr{MklInt})::Cvoid
 
-perm = MklInt[]
-iparm = zeros(MklInt, 64)
 
 @ccall "./bin/mkl_rt.2.dll".pardiso(
   pt::Ptr{Int64},
